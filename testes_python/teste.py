e5d5daaaa18b65969599e989c32bd4b0bb7c494b
andersmotta <andersmotta@hotmail.com>
2022-02-02 20:38:41 -0300

    instruções para realizar o commit todo pelo vscode

--- testes_python/teste.py
+++ testes_python/teste.py
@@ -5,3 +5,14 @@ print("Este é o meu primeiro teste com GitHub")
 print("Eu estou editando o segundo comando em python para testar push / pull")
 
 print("Este terceiro trecho foi commitado somente pelo VSCode")
+
+
+
+
+# trecho onde todo o processo é realizado somente pelo VSCode
+# passo a passo
+
+# 1 - Editar e salvar o arquivo fonte
+# 2 - Efetivar o arquivo na opção 'alterações'
+# 3 - Realizar o Commit com um comentário em texto
+# 4 - Realizar o sync pelo VSCode na barra azul no canto esquerdo inferior
